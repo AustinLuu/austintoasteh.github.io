0a862ae7d2d73a52067c14c8b7301786946ff312
Austin Luu <32989647+austintoasteh@users.noreply.github.com>
2018-04-04 00:12:18 -0400

    Add files via upload

---
+++ AustinLuu/js/text_type.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    Typed.new("#typed", {
+        stringsElement: document.getElementById('typed-strings'),
+        typeSpeed: 100,
+        startDelay: 500,
+        backDelay: 500,
+        loop: false,
+        showCursor: true,
+        cursorChar: "|",
+        contentType: 'html', // or text
+        // defaults to null for infinite loop
+        loopCount: null,
+        callback: function(){ foo(); },
+        resetCallback: function() { newTyped(); }
+    });
+
+    var resetElement = document.querySelector('.reset');
+    if(resetElement) {
+        resetElement.addEventListener('click', function() {
+            document.getElementById('typed')._typed.reset();
+        });
+    }
+
+});
+
+function newTyped(){ /* A new typed object */ }
+
+function foo(){ console.log("Callback"); }
